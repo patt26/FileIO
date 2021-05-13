b433075aff760c6da63856573029b3d1afc8a85e
patt26 <kharchepratik123@gmail.com>
2021-05-13 11:09:33 +0530

    Added File I/O

---
+++ FileIO/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileExists();
+            //ReadAllLines();
+            //ReadAllText();
+            //FileCopy();
+            // DeleteFile();
+            ReadFromStreamReader();
+            WriteUsingStreamWriter();
+
+        }
+        public static void FileExists()
+        {
+            String path = "Example.txt";
+            if (File.Exists(path))
+                Console.WriteLine("File exists");
+            else
+                Console.WriteLine("File doesnt exist");
+        }
+        public static void ReadAllLines()
+        {
+            string path = @"C:\Users\prat\source\repos\FileIO\FileIO\Example.txt";
+            string[] lines;
+
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+        }
+        public static void ReadAllText()
+        {
+            string path = @"C:\Users\prat\source\repos\FileIO\FileIO\Example.txt";
+            string lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+        } 
+        public static void FileCopy()
+        {
+            string path = @"C:\Users\prat\source\repos\FileIO\FileIO\Example.txt";
+            string copypath = @"C:\Users\prat\source\repos\FileIO\FileIO\ExampleNew.txt";
+            File.Copy(path, copypath);
+        }
+        public static void DeleteFile()
+        {
+            string path = @"C:\Users\prat\source\repos\FileIO\FileIO\Example.txt";
+            File.Delete(path);
+        }
+        public static void ReadFromStreamReader()
+        {
+            string path = @"C:\Users\prat\source\repos\FileIO\FileIO\ExampleNew.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine())!= null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+        public static void WriteUsingStreamWriter()
+        {
+            string path = @"C:\Users\prat\source\repos\FileIO\FileIO\ExampleNew.txt";
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hello World .Net is awesome");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
---
+++ SerializationDemo/BinaryDeSerialization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationDemo
+{
+    class BinaryDeSerialization
+    {
+        public void DeSerialization()
+        {
+            System.IO.FileStream fileStream = new FileStream(@"/Example.txt", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Demo deserializationdemo = (Demo)formatter.Deserialize(fileStream);
+            Console.WriteLine($"Application name {deserializationdemo.ApplicationName} ---ApplicationId    {deserializationdemo.ApplicationId}");
+            Console.ReadKey();
+        }
+    }
+
+}
